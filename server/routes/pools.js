b6a2290c9229a6b610edf73820bc831d93c38c54
ace1122sp <ace11.space22@gmail.com>
2018-07-24 20:46:37 +0200

    extracted error middleware

--- server/routes/pools.js
+++ server/routes/pools.js
@@ -1,37 +1,31 @@
 const express = require('express');
-const { validationResult } = require('express-validator/check');
-const { validatePool, validatePoolId, validateFollowerId, validateOptionId, validateOption, validateOptions } = require('../controllers/poolValidators');
 
+const { validatePool, validatePoolId, validateFollowerId, validateOptionId, validateOption, validateOptions } = require('../controllers/poolValidators');
 const controllers = require('../controllers/poolControllers');
+const { errorResponse } = require('../controllers/errorControllers');
 
 const poolsRouter = express.Router();
 
-const _validatorErrorResponse = (req, res, next) => {
-  const errors = validationResult(req);
-  if (!errors.isEmpty()) return res.status(422).json({ errors: errors.array() });
-  next();
-}
-
 poolsRouter.route('/')
   .get(controllers.getPools) 
-  .post(validatePool, validateOptions, _validatorErrorResponse, controllers.createPool);
+  .post(validatePool, validateOptions, errorResponse, controllers.createPool);
 
 poolsRouter.route('/:poolId')
-  .get(validatePoolId, _validatorErrorResponse, controllers.getPool) 
-  .delete(validatePoolId, _validatorErrorResponse, controllers.deletePool);
+  .get(validatePoolId, errorResponse, controllers.getPool) 
+  .delete(validatePoolId, errorResponse, controllers.deletePool);
   
-poolsRouter.patch('/:poolId/votes', validatePoolId, validateOptionId, _validatorErrorResponse, controllers.vote);
+poolsRouter.patch('/:poolId/votes', validatePoolId, validateOptionId, errorResponse, controllers.vote);
 
 poolsRouter.route('/:poolId/followers')
-  .put(validatePoolId, validateFollowerId, _validatorErrorResponse, _validatorErrorResponse, controllers.followPool); 
+  .put(validatePoolId, validateFollowerId, errorResponse, controllers.followPool); 
 
 poolsRouter.route('/:poolId/followers/:followerId')
-  .delete(validatePoolId, validateFollowerId, _validatorErrorResponse, controllers.unfollowPool);
+  .delete(validatePoolId, validateFollowerId, errorResponse, controllers.unfollowPool);
 
 poolsRouter.route('/:poolId/options')
-  .put(validatePoolId, validateOption, _validatorErrorResponse, controllers.addOption);
+  .put(validatePoolId, validateOption, errorResponse, controllers.addOption);
 
 poolsRouter.route('/:poolId/options/:optionId')
-  .delete(validatePoolId, validateOptionId, _validatorErrorResponse, controllers.removeOption);
+  .delete(validatePoolId, validateOptionId, errorResponse, controllers.removeOption);
 
 module.exports = poolsRouter;
