78ede4d0e544dbc1f20244cdbb01ddb21dbeff7d
ace11 <ace11.space22@gmail.com>
2018-08-02 23:27:15 +0200

    added shadow user actions

---
+++ client/actions/user.shadow.js
@@ -0,0 +1,42 @@
+import { LOAD_USER, UNLOAD_USER, ADD_TO_CREATED_POOLS, REMOVE_FROM_CREATED_POOLS, FOLLOW_POOL, UNFOLLOW_POOL } from '../constants';
+
+export const loadUser = user => {
+  return {
+    type: LOAD_USER,
+    user
+  }
+}
+
+export const unloadUser = () => {
+  return {
+    type: UNLOAD_USER
+  }
+}
+
+export const addToCreatedPools = poolCard => {
+  return {
+    type: ADD_TO_CREATED_POOLS,
+    poolCard
+  }
+}
+
+export const removeFromCreatedPools = poolId => {
+  return {
+    type: REMOVE_FROM_CREATED_POOLS,
+    poolId
+  }
+}
+
+export const followPool = poolCard => {
+  return {
+    type: FOLLOW_POOL,
+    poolCard
+  }
+}
+
+export const unfollowPool = poolId => {
+  return {
+    type: UNFOLLOW_POOL,
+    poolId
+  }
+}
