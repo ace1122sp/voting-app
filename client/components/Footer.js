165e188a591911c45ac619fa616cc38e907f3cc4
ace11 <ace11.space22@gmail.com>
2018-08-28 00:20:29 +0200

    css: added icons to footer; header fixed to top

--- client/components/Footer.js
+++ client/components/Footer.js
@@ -14,12 +14,12 @@ const Footer = props => {
         <address>
           <p>coded by ace11</p>
         </address>
-        <ul>
-          <li><a href="https://github.com/ace1122sp" target="_blank"><i className="fa fa-github" aria-hidden="true"></i>github</a></li>
-          <li><a href="https://www.linkedin.com/in/aleksandar-bulovi%C4%87-83aa74139/" target="_blank"><i className="fa fa-linkedin" aria-hidden="true"></i>linkedin</a></li>
-          <li><a href="https://www.freecodecamp.com/ace1122sp" target="_blank"><i className="fa fa-free-code-camp" aria-hidden="true"></i>freecodecamp</a></li>
-          <li><a href="https://codepen.io/ace1122/" target="_blank"><i className="fa fa-codepen" aria-hidden="true"></i>codepen</a></li>
-          <li><a href='https://ace1122sp.github.io/portfolio/' target="_blank">portfolio</a></li>
+        <ul className='social-media'>
+          <li><a href='https://github.com/ace1122sp' target='_blank'><i className='fa fa-github' aria-hidden='true'></i> github</a></li>
+          <li><a href='https://www.linkedin.com/in/aleksandar-bulovi%C4%87-83aa74139/' target='_blank'><i className='fa fa-linkedin' aria-hidden='true'></i> linkedin</a></li>
+          <li><a href='https://www.freecodecamp.com/ace1122sp' target='_blank'><i className='fa fa-free-code-camp' aria-hidden='true'></i> freecodecamp</a></li>
+          <li><a href='https://codepen.io/ace1122/' target='_blank'><i className='fa fa-codepen' aria-hidden='true'></i> codepen</a></li>
+          <li><a href='https://ace1122sp.github.io/portfolio/' target='_blank'><i className='fa fa-code' aria-hidden='true'></i> portfolio</a></li>
         </ul>
       </section>
     </footer>
